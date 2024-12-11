e32bfb0a54d8652fdc72980b41948cd90fcda60b
tsoumagas-benjamin <bptsoumagas@gmail.com>
2024-12-11 01:13:55 -0500

    Added a delete function, and improved styling

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Use DOM manipulation to select each of the elements for the API calls
 const fetchButton = document.getElementById("fetch");
 const XHRButton = document.getElementById("XHR");
+const deleteButton = document.getElementById("delete-button");
+const deletionIndex = document.getElementById("deletion-index");
 const dataDisplay = document.getElementById("data-display");
 const dataHeader = document.getElementById("data-header");
 const dataForm = document.getElementById("data-form");
@@ -64,12 +66,40 @@ function getXHRData() {
     request.send();
 }
 
+//Function to delete data using DELETE requests and display if we are successful
+function deleteXHRData() {
+    let target = "https://jsonplaceholder.typicode.com/posts";
+    if (deletionIndex.value) {
+        target = target + `/${deletionIndex.value}`;
+        // Initialize a DELETE request with a XHR object
+        const request = new XMLHttpRequest();
+        request.open("DELETE", target);
+        // Handle abort, error, or timeouts
+        request.addEventListener('abort', () => {
+            dataHeader.innerText = "Request Aborted!";
+            dataDisplay.innerText = "XMLHTTPRequest was aborted!";
+        });
+        request.addEventListener('error', () => {
+            dataHeader.innerText = "Error!";
+            dataDisplay.innerText = "XMLHTTPRequest not completed!";
+        });
+        request.addEventListener('timeout', () => {
+            dataHeader.innerText = "Request Timed Out!";
+            dataDisplay.innerText = "XMLHTTPRequest was timed out!";
+        });
+        // Alert user the post has been deleted
+        alert(`Post ${deletionIndex.value} was updated!`);
+    } else {
+        alert("Cannot delete post without specified ID!");
+    }
+}
+
 //Function to post data using PUT/POST requests and display if we are successful
 async function postData() {
     let target = "https://jsonplaceholder.typicode.com/posts";
     // If user provides an ID to replace, perform a PUT request with XMLHTTPRequests instead
-    if (userID.innerHTML) {
-        target = target + `/${userID.innerHTML}`;
+    if (userID.value) {
+        target = target + `/${userID.value}`;
         // Initialize a GET request with a XHR object
         const request = new XMLHttpRequest();
         request.open("PUT", target);
@@ -88,10 +118,10 @@ async function postData() {
         });
         // On load, parse response as a JSON object for title and body; display it
         request.addEventListener('load', () => {
-            console.log(request.responseText);
+            alert(`Post ${userID.value} was updated!`);
         });
         // Initiate the PUT request with the content from the user
-        request.send(JSON.stringify({title: userTitle, body: userBody}));
+        request.send(JSON.stringify({id: userID.value, title: userTitle, body: userBody}));
     // If there is no ID provided, do a POST request with fetch()
     } else {
         try {
@@ -121,4 +151,5 @@ async function postData() {
 
 fetchButton.addEventListener('click', getData);
 XHRButton.addEventListener('click', getXHRData);
-dataForm.addEventListener('submit', postData);
+deleteButton.addEventListener('click', deleteXHRData);
+submitForm.addEventListener('click', postData);
